2d4e1e1e7bd80edf0ed5245ba2cf4cf766c40330
Vladimir Grichina <vgrichina@gmail.com>
2021-10-12 21:41:19 -0700

    Update query a bit

--- sync.sh
+++ sync.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
 QUESTDB_URL=http://35.203.150.125:9000
-PG_URL=postgres://public_readonly:nearprotocol@104.199.89.51/mainnet_explorer
+PG_URL=${PG_URL:=postgres://public_readonly:nearprotocol@104.199.89.51/mainnet_explorer}
+
+set -ex
+# echo $PG_URL
 
 ROW_LIMIT=1000
 # TODO: Update query so that rows at the limit edge don't get dropped (because of timestamp filtering)
 
-LATEST=$(curl -G \
-  --data-urlencode "query=select cast(max(ts) as long) from 1.csv" \
-  "$QUESTDB_URL/exec" | jq '.dataset[0][0] // 0' )
-echo "Latest: $LATEST"
+LATEST_JSON=$(curl -G \
+  --data-urlencode "query=select cast(ts as long), index_in_chunk from actions order by ts desc, index_in_chunk desc limit 1" \
+  "$QUESTDB_URL/exec")
+
+LATEST_TIMESTAMP=$(echo $LATEST_JSON | jq '.dataset[0][0] // 0')
+LATEST_INDEX=$(echo $LATEST_JSON | jq '.dataset[0][1] // 0')
+
+echo "Latest: $LATEST $LATEST_TIMESTAMP $LATEST_INDEX"
 
 # | curl -F data=@- /imp << EOF
-time psql $PG_URL --csv > 1.csv << EOF
+# cat << EOF
+# time psql $PG_URL --csv > tmp.csv << EOF
+cat << EOF
 
 select * from ( 
 	select
-		to_char(to_timestamp(receipt_included_in_block_timestamp / 1000000000), 'yyyy-MM-dd"T"HH24:MI:SS.US"Z"') as ts,
+		to_char(to_timestamp(transactions.block_timestamp / 1000000000), 'yyyy-MM-dd"T"HH24:MI:SS.US"Z"') as ts,
 		receipt_id,
 		index_in_action_receipt,
 		action_kind,
@@ -24,21 +33,25 @@ select * from (
 		args->>'method_name' as method_name,
 		args->>'args_json' as args_json,
 		args->'args_json'->>'receiver_id' as args_receiver_id,
-		receipt_receiver_account_id,
-		receipt_predecessor_account_id,
+		receipts.receiver_account_id,
+		receipts.predecessor_account_id,
 		transaction_hash,
+		transactions.index_in_chunk,
 		signer_account_id,
 		signer_public_key
-	from action_receipt_actions
-	join receipts using (receipt_id)
-	join transactions on originated_from_transaction_hash = transaction_hash
-    where receipt_included_in_block_timestamp > $LATEST * 1000
-	order by receipt_included_in_block_timestamp
-	limit $ROW_LIMIT
+	from (
+		select * from transactions
+		where transactions.block_timestamp > $LATEST_TIMESTAMP * 1000 
+			-- or (transactions.block_timestamp / 1000 = $LATEST_TIMESTAMP and transactions.index_in_chunk > $LATEST_INDEX)
+		order by transactions.block_timestamp, transactions.index_in_chunk
+		limit $ROW_LIMIT
+	) as transactions
+	join receipts on originated_from_transaction_hash = transaction_hash
+	join action_receipt_actions using (receipt_id)
 ) filtered
 order by ts, index_in_action_receipt
 
 EOF
 
-time curl -F data=@1.csv "$QUESTDB_URL/imp"
+# time curl -F data=@tmp.csv "$QUESTDB_URL/imp?name=actions"
 
